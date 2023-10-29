5d1bf4d9b5616964b89b918fba04189355d96147
tomoka115 <tmkhreuph@gmail.com>
2023-10-29 09:37:39 +0900

    ポートフォリオサイトを作成

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-function checkAnswer() {
-    const movieChoice = document.getElementById("movieChoice");
-    const quizResult = document.getElementById("quizResult");
-    const quizSection = document.getElementById("quizSection");
-    const hobbySection = document.getElementById("hobbySection");
-    const resultSection = document.getElementById("resultSection");
+const movieChoice = document.getElementById("movieChoice");
+const quizResult = document.getElementById("quizResult");
+const quizSection = document.getElementById("quizSection");
+const hobbySection = document.getElementById("hobbySection");
 
-    if (movieChoice.style.backgroundColor === "") {
-      quizResult.textContent = "正解！私の趣味は映画鑑賞です。";
-      quizResult.style.color = "green";
-      quizSection.classList.add("hidden");
-      hobbySection.classList.remove("hidden");
-      resultSection.classList.remove("hidden");
-    } else {
-      quizResult.textContent = "残念！不正解です。";
-      quizResult.style.color = "red";
-      resultSection.classList.remove("hidden");
-    }
-  }
+movieChoice.onclick = function () {
+  quizResult.textContent = "正解！わたしの趣味は映画鑑賞です。"
+  hobbySection.classList.remove("hidden");
+  quizSection.classList.add("hidden");
+}
+
+const walkChoice = document.getElementById("walkChoice")
+walkChoice.onclick = function () {
+  quizResult.textContent = "残念！不正解です。"
+}
+
+const bakingChoice = document.getElementById("bakingChoice")
+bakingChoice.onclick = function () {
+  quizResult.textContent = "残念！不正解です。"
+}
